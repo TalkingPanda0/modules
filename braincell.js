0ee3404e99a4e445b07ed850819f104d278ef3b1
Talking Panda <memretwitch3626@gmail.com>
2023-07-29 17:27:41 +0300

    a

--- braincell.js
+++ braincell.js
@@ -1,13 +1,14 @@
 var oldtitle="";
 var title = "";
-var poll = "";
+//var poll = "";
 
 function temptitle(ttitle)
 {
 	oldtitle = $.getStatus($.channelName);
+
 	$.updateStatus($.channelName, ttitle , "braincell",1);
 	setTimeout(undotitle, 900000);
-	$.say("Changed title to \"" + ttile + "\" for 15 minutes ");
+	$.say("Changed title to \"" + ttitle + "\" for 15 minutes ");
 }
 function undotitle(){
 	$.updateStatus($.channelName, oldtitle , "braincell",1);
@@ -16,23 +17,29 @@ function undotitle(){
 	oldtitle = ""
 
 }
-function parsepoll(poll)
+/*function parsepoll(ipoll)
 {
+	// format Q: [question] A: [answer1] A: [answer2] ...
 	// poll
 	poll = ""
-}
+}*/
 (function () {
 	$.bind('command', function (event) {
+		
 
 		var sender = event.getSender(),
 			command = event.getCommand(),
 			args = event.getArgs();
 		if(command.equalsIgnoreCase("approvetitle"))
 			temptitle(title);
-		else if(command.equalsIgnoreCase("approvepoll"))
+		/*else if(command.equalsIgnoreCase("approvepoll"))
 			parsepoll(poll);
+		else if(command.equalsIgnoreCase("poll")
+			parsepoll(args.join(" "));*/
 		else
+		{
 			temptitle(args.join(" "));
+		}
 	 
 	});
 	
@@ -42,20 +49,16 @@ function parsepoll(poll)
             userInput = event.getUserInput(),
 	    userName = event.getUsername();
 	if( rewardTitle.equalsIgnoreCase("Self Timeout")){
-		$.say("0");
 		$.say(".timeout " + userName + " 300" + " Self Timeout");
 	}
 	else if( rewardTitle.equalsIgnoreCase("Timeout Somebody Else")){
-		$.say("1");
 		$.say(".timeout " + userInput + " 60" + " Timeout request by " + userName);
 	}
-	else if( rewardTitle.equalsIgnoreCase("Poll")){
-		$.say("2");
+	/*else if( rewardTitle.equalsIgnoreCase("Poll")){
 		if(poll === "")
 			poll = userInput;
-	}
+	}*/
 	else if( rewardTitle.equalsIgnoreCase("CHANGE TITLE FOR 15m")){
-		$.say("3");
 		if(title === "")
 			title = userInput;
 	}
@@ -66,8 +69,9 @@ function parsepoll(poll)
 	*/
 	$.bind('initReady', function () {
 		$.registerChatCommand('./custom/braincell.js', 'approvetitle', 2);
-		$.registerChatCommand('./custom/braincell.js', 'approvepoll', 2);
+		//$.registerChatCommand('./custom/braincell.js', 'approvepoll', 2);
 		$.registerChatCommand('./custom/braincell.js', 'temptitle', 2);
+		//$.registerChatCommand('./custom/braincell.js', 'poll', 2);
 	});
 
 })();
